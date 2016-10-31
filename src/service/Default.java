1ca8c536bd81732698ea302610eed97af11f57a5
Henrik Lawall <henrik@henrik.com>
2016-10-31 13:18:22 +0100

    Added aggregation

--- src/service/Default.java
+++ src/service/Default.java
@@ -1,6 +1,7 @@
 package service;
 
 import com.datastax.driver.core.*;
+import com.datastax.driver.core.querybuilder.QueryBuilder;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -15,10 +16,10 @@ public class Default {
     public String get() {
         CassandraConnection connection = new CassandraConnection("141.19.145.144", "keyspace2");
         Session session = connection.getSession();
-        ResultSet results = session.execute("SELECT * FROM test");
+        ResultSet results = session.execute("SELECT sum(age) AS summe FROM test");
         StringBuilder sb = new StringBuilder();
         for (Row row: results) {
-            sb.append(row.getString("name") + ": " + row.getInt("age") + "\n");
+            sb.append(row.getInt("summe") + "\n");
         }
         connection.closeConnection();
         return sb.toString();
