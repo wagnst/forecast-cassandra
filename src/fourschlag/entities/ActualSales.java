f12e908d36641a8ebfbef9f6784d52364791e6d9
Henrik Lawall <henrik@henrik.com>
2016-11-04 14:33:38 +0100

    Added class for Sales Table

---
+++ src/fourschlag/entities/ActualSales.java
@@ -0,0 +1,16 @@
+package fourschlag.entities;
+
+import com.datastax.driver.mapping.annotations.Table;
+
+@Table(keyspace = "", name = "actual_sales")
+public class ActualSales {
+    //Attribute
+
+    public ActualSales() {
+        //Attribute setzen
+    }
+
+    //Getter
+
+    //Equals und Hashcode
+}
