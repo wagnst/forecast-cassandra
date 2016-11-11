83627588daa49cdf10aed6c0f91531bd36fbc927
Henrik Lawall <henrik.lawall@web.de>
2016-11-11 18:18:26 +0100

    Added KPIs price, var_cost, cm1_specific and cm1_percent

--- src/fourschlag/services/data/SalesService.java
+++ src/fourschlag/services/data/SalesService.java
@@ -10,6 +10,7 @@ import fourschlag.entities.accessors.ForecastSalesAccessor;
 import fourschlag.entities.accessors.OrgStructureAccessor;
 import fourschlag.entities.accessors.RegionAccessor;
 import fourschlag.entities.types.*;
+import jnr.ffi.annotations.Out;
 
 import java.time.LocalDateTime;
 import java.util.*;
@@ -71,19 +72,28 @@ public class SalesService extends Service {
           * we are accessing the data */
         Period planPeriod = Period.getPeriodByYear(planYear);
 
-        //System.out.println(productMainGroup + " " + requestedPeriod + " " + region + " " + salesType);
+        //System.out.println(productMainGroup + " " + planPeriod + " " + region + " " + salesType);
 
+        /* Create Lists for each KPI to store the values of the 18 months */
         LinkedList<Double> salesVolumesMonths = new LinkedList<>();
         LinkedList<Double> netSalesMonths = new LinkedList<>();
         LinkedList<Double> cm1Months = new LinkedList<>();
+        LinkedList<Double> priceMonths = new LinkedList<>();
+        LinkedList<Double> varCostMonths = new LinkedList<>();
+        LinkedList<Double> cm1SpecificMonths = new LinkedList<>();
+        LinkedList<Double> cm1PercentMonths = new LinkedList<>();
 
         SalesEntity queryResult;
         boolean actualFlag = false;
         boolean forecastFlag = false;
 
+        double currentSalesVolume;
+        double currentNetSales;
+        double currentCm1;
+
         for (int i = 0; i < MONTHS_AMOUNT; i++) {
             /* Compare plan period with current period
-               If plan period in history (plan: 201610, current: 201611) use actual data.
+               If plan period in past (plan: 201610, current: 201611) use actual data.
                Else forecast data
              */
             /* TODO: IF planPeriod is currentPeriod - 1, THEN try to get actual data. IF actual data is not available, THEN use forecast data */
@@ -102,22 +112,40 @@ public class SalesService extends Service {
                         planPeriod.getPeriod(), region, salesType.toString());
             }
 
-            /* Check if queryResult has data */
+            /* Check if queryResult has data and save the data temporarily because we need the values again for calculations */
             if (queryResult == null) {
-                salesVolumesMonths.add(new Double(0));
-                netSalesMonths.add(new Double(0));
-                cm1Months.add(new Double(0));
+                currentSalesVolume = 0;
+                currentNetSales = 0;
+                currentCm1 = 0;
             } else {
-                salesVolumesMonths.add(queryResult.getSalesVolumes());
-                netSalesMonths.add(queryResult.getNetSales());
-                cm1Months.add(queryResult.getCm1());
+                currentSalesVolume = queryResult.getSalesVolumes();
+                currentNetSales = queryResult.getNetSales();
+                currentCm1 = queryResult.getCm1();
             }
+
+            /* IF sales volume is 0 THEN set these KPIs also to 0. ELSE calculate the other KPIs*/
+            if(currentSalesVolume == 0) {
+                priceMonths.add(new Double(0));
+                varCostMonths.add(new Double(0));
+                cm1SpecificMonths.add(new Double(0));
+                cm1PercentMonths.add(new Double(0));
+            } else {
+                priceMonths.add(currentNetSales/currentSalesVolume*1000);
+                varCostMonths.add((currentNetSales-currentCm1)*1000/currentSalesVolume);
+                cm1SpecificMonths.add(currentCm1/currentSalesVolume*1000);
+                cm1PercentMonths.add(currentCm1/currentNetSales*100);
+            }
+
+            /* Put the temporarily saved KPIs into the List*/
+            salesVolumesMonths.add(currentSalesVolume);
+            netSalesMonths.add(currentNetSales);
+            cm1Months.add(currentCm1);
 
             /* Increment the planPeriod to jump to the next month */
             planPeriod.increment();
         }
 
-        /* Check flags to decide what entry type was used */
+        /* Check flags to set what entry type was used */
         EntryType entryType;
         if (actualFlag && forecastFlag) {
             entryType = EntryType.ACTUAL_FORECAST;
@@ -127,17 +155,37 @@ public class SalesService extends Service {
             entryType = EntryType.ACTUAL;
         }
 
+        // TODO: Think about putting all these objects into Lists and then be able to iterate over them. Less performant but nicer to look at.
         /* Create Objects for each KPI */
-        OutputDataType salesVolume = new OutputDataType(KeyPerformanceIndicators.SALES_VOLUME, sbu, productMainGroup, region, region,
-                salesType.toString(), entryType.toString(), salesVolumesMonths);
-        OutputDataType netSales = new OutputDataType(KeyPerformanceIndicators.NET_SALES, sbu, productMainGroup, region, region,
-                salesType.toString(), entryType.toString(), netSalesMonths);
-        OutputDataType cm1 = new OutputDataType(KeyPerformanceIndicators.CM1, sbu, productMainGroup, region, region, salesType.toString(),
-                entryType.toString(), cm1Months);
+        OutputDataType salesVolumes = new OutputDataType(KeyPerformanceIndicators.SALES_VOLUME, sbu, productMainGroup,
+                region, region, salesType.toString(), entryType.toString(), salesVolumesMonths);
 
-        resultList.add(salesVolume);
+        OutputDataType netSales = new OutputDataType(KeyPerformanceIndicators.NET_SALES, sbu, productMainGroup, region,
+                region, salesType.toString(), entryType.toString(), netSalesMonths);
+
+        OutputDataType cm1 = new OutputDataType(KeyPerformanceIndicators.CM1, sbu, productMainGroup, region, region,
+                salesType.toString(), entryType.toString(), cm1Months);
+
+        OutputDataType price = new OutputDataType(KeyPerformanceIndicators.PRICE, sbu, productMainGroup, region, region,
+                salesType.toString(), entryType.toString(), priceMonths);
+
+        OutputDataType varCost = new OutputDataType(KeyPerformanceIndicators.VAR_COSTS, sbu, productMainGroup, region,
+                region, salesType.toString(), entryType.toString(), varCostMonths);
+
+        OutputDataType cm1Specific = new OutputDataType(KeyPerformanceIndicators.CM1_SPECIFIC, sbu, productMainGroup,
+                region, region, salesType.toString(), entryType.toString(), cm1SpecificMonths);
+
+        OutputDataType cm1Percent = new OutputDataType(KeyPerformanceIndicators.CM1_PERCENT, sbu, productMainGroup,
+                region, region, salesType.toString(), entryType.toString(), cm1PercentMonths);
+
+        /* Add all KPI Objects to the list that is returned by this method */
+        resultList.add(salesVolumes);
         resultList.add(netSales);
         resultList.add(cm1);
+        resultList.add(price);
+        resultList.add(varCost);
+        resultList.add(cm1Specific);
+        resultList.add(cm1Percent);
 
         return resultList;
     }
