1f3e39547202145ce27054b6c757f5c07039ec0a
Henrik Lawall <henrik.lawall@web.de>
2016-11-14 18:10:59 +0100

    Fixed wrong return type

--- src/fourschlag/entities/accessors/ForecastSalesAccessor.java
+++ src/fourschlag/entities/accessors/ForecastSalesAccessor.java
@@ -3,12 +3,12 @@ package fourschlag.entities.accessors;
 import com.datastax.driver.mapping.annotations.Accessor;
 import com.datastax.driver.mapping.annotations.Param;
 import com.datastax.driver.mapping.annotations.Query;
-import fourschlag.entities.ActualSalesEntity;
+import fourschlag.entities.ForecastSalesEntity;
 
 @Accessor
 public interface ForecastSalesAccessor {
     @Query("SELECT sales_volumes, net_sales, cm1 FROM forecast_sales WHERE product_main_group = :product_main_group AND period = :period AND plan_period = :plan_period AND region = :region AND sales_type = :sales_type ALLOW FILTERING;")
-    ActualSalesEntity getSalesKPI(
+    ForecastSalesEntity getSalesKPI(
             @Param("product_main_group") String productMainGroup,
             @Param("period") int period,
             @Param("plan_period") int planPeriod,
@@ -16,7 +16,7 @@ public interface ForecastSalesAccessor {
             @Param("sales_type") String salesType);
 
     @Query("SELECT cm1 FROM forecast_sales WHERE product_main_group = :product_main_group AND period = :period AND plan_period = :plan_period AND region = :region AND sales_type = :sales_type ALLOW FILTERING;")
-    ActualSalesEntity getCm1(
+    ForecastSalesEntity getCm1(
             @Param("product_main_group") String productMainGroup,
             @Param("period") int period,
             @Param("plan_period") int planPeriod,
