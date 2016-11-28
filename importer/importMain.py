dd693406819d9a5f941c3ae27d610f0538fa21a0
Rahtainka <rahtainka@t-online.de>
2016-11-28 18:00:23 +0100

    fixed some bugs added config file moved table configs to config file

--- importer/importMain.py
+++ importer/importMain.py
@@ -13,10 +13,13 @@ import subprocess
 import time
 import shutil
 import multiprocessing
+import json
+
+with open('config.json') as json_data_file:
+    config = json.load(json_data_file)
 
 # Konstanten, die je nachdem wo das Script liegt befüllt werden müssen:
-PATH_CSV_OUTPUT = os.path.dirname(__file__) + '/csv_output/'
-CQLSH_BINARY = "/usr/local/bin/cqlsh"
+CQLSH = config['cqlsh_binary']
 
 # Konstanten
 INTERACTIVE_MODE = "-e"
@@ -24,70 +27,8 @@ USED_KEYSPACE = '-k'
 USER = '--username='
 PASSWORD = '--password='
 
-ORG_STRUCTURE_PARAMS = 'bu, sbu, product_main_group'
-REGIONS_PARAMS = 'region, subregion'
-EXCHANGE_RATE_PARAMS = 'period, period_year, period_month, from_currency, to_currency, rate, userid, entry_ts'
-ACTUAL_SALES_PARAMS = 'sales_volumes, net_sales, cm1, product_main_group, region, sbu, sales_type, data_source, period, ' \
-                      'period_year, period_half_year, period_quarter, period_month, currency, userid, entry_ts'
-FORECAST_SALES_PARAMS = 'sales_volumes, net_sales, cm1, topdown_adjust_sales_volumes, topdown_adjust_net_sales, ' \
-                        'topdown_adjust_cm1, product_main_group, region, sales_type, entry_type, period, period_year, ' \
-                        'period_month, plan_period, plan_year, plan_half_year, plan_quarter, plan_month, currency, status, ' \
-                        'usercomment, userid, entry_ts'
-ACTUAL_FIXED_COSTS_PARAMS = "fix_pre_man_cost, ship_cost, sell_cost, diff_act_pre_man_cost, idle_equip_cost, rd_cost, " \
-                            "admin_cost_bu, admin_cost_od, other_op_cost_bu, other_op_cost_od, spec_items, provisions, " \
-                            "currency_gains, val_adjust_inventories, other_fix_cost, depreciation, cap_cost, sbu, " \
-                            "region, subregion, period, period_year, period_half_year, period_quarter, period_month, " \
-                            "currency, userid, entry_ts, admin_cost_company, other_op_cost_company, equity_income"
-FORECAST_FIXED_COSTS_PARAMS = "fix_pre_man_cost, ship_cost, sell_cost, diff_act_pre_man_cost, idle_equip_cost, " \
-                              "rd_cost, admin_cost_bu, admin_cost_od, other_op_cost_bu, other_op_cost_od, spec_items, " \
-                              "provisions, currency_gains, val_adjust_inventories, other_fix_cost, " \
-                              "topdown_adjust_fix_costs, depreciation, cap_cost, sbu, region, subregion, " \
-                              "entry_type, period, period_year, period_month, plan_period, plan_year, plan_half_year, " \
-                              "plan_quarter, plan_month, currency, status, usercomment, userid, entry_ts, " \
-                              "admin_cost_company, other_op_cost_company, equity_income"
-
-ORG_STRUCTURE_TABLE_CREATION = '(BU varchar, SBU varchar, PRODUCT_MAIN_GROUP varchar PRIMARY KEY);'
-REGIONS_TABLE_CREATION = '(REGION varchar, SUBREGION varchar PRIMARY KEY)'
-EXCHANGE_RATE_TABLE_CREATION = '(PERIOD int, PERIOD_YEAR int, PERIOD_MONTH int, FROM_CURRENCY varchar, ' \
-                               'TO_CURRENCY varchar, RATE double, USERID varchar, ENTRY_TS varchar, ' \
-                               'PRIMARY KEY (PERIOD, FROM_CURRENCY, TO_CURRENCY));'
-ACTUAL_SALES_TABLE_CREATION = '(SALES_VOLUMES double, NET_SALES double, CM1 double, PRODUCT_MAIN_GROUP varchar, ' \
-                              'REGION varchar, SBU varchar, SALES_TYPE varchar, DATA_SOURCE varchar, PERIOD int, ' \
-                              'PERIOD_YEAR int, PERIOD_HALF_YEAR int, PERIOD_QUARTER int, PERIOD_MONTH int, ' \
-                              'CURRENCY varchar, USERID varchar, ENTRY_TS varchar, ' \
-                              'PRIMARY KEY ((PRODUCT_MAIN_GROUP, Region), PERIOD, SALES_TYPE, DATA_SOURCE));'
-FORECAST_SALES_TABLE_CREATION = '(SALES_VOLUMES double, NET_SALES double, ' \
-                                'CM1 double, PRODUCT_MAIN_GROUP varchar, REGION varchar, SALES_TYPE varchar, ' \
-                                'PERIOD int, PERIOD_YEAR int, PERIOD_MONTH int, CURRENCY varchar, USERID varchar, ' \
-                                'ENTRY_TS varchar, TOPDOWN_ADJUST_SALES_VOLUMES double, ' \
-                                'TOPDOWN_ADJUST_NET_SALES double, TOPDOWN_ADJUST_CM1 double, PLAN_PERIOD int, ' \
-                                'PLAN_MONTH int, PLAN_YEAR int, PLAN_HALF_YEAR int, PLAN_QUARTER int, STATUS varchar, ' \
-                                'USERCOMMENT text, ENTRY_TYPE varchar, PRIMARY KEY ((PRODUCT_MAIN_GROUP, REGION), ' \
-                                'PERIOD, SALES_TYPE, PLAN_PERIOD, ENTRY_TYPE));'
-ACTUAL_FIXED_COSTS_TABLE_CREATION = '(FIX_PRE_MAN_COST double, SHIP_COST double, SELL_COST double, ' \
-                                    'DIFF_ACT_PRE_MAN_COST double, IDLE_EQUIP_COST double, RD_COST double, ' \
-                                    'ADMIN_COST_BU double, ADMIN_COST_OD double, OTHER_OP_COST_BU double, ' \
-                                    'OTHER_OP_COST_OD double, SPEC_ITEMS double, PROVISIONS double, ' \
-                                    'CURRENCY_GAINS double, VAL_ADJUST_INVENTORIES double, OTHER_FIX_COST double, ' \
-                                    'DEPRECIATION double, CAP_COST double, SBU varchar, REGION varchar, ' \
-                                    'SUBREGION varchar, PERIOD int, PERIOD_YEAR int, PERIOD_HALF_YEAR int, ' \
-                                    'PERIOD_QUARTER int, PERIOD_MONTH int, CURRENCY varchar, USERID varchar, ' \
-                                    'ENTRY_TS varchar, ADMIN_COST_COMPANY double, OTHER_OP_COST_COMPANY double, ' \
-                                    'EQUITY_INCOME double, PRIMARY KEY ((SBU, SUBREGION), PERIOD));'
-FORECAST_FIXED_COSTS_TABLE_CREATION = '(FIX_PRE_MAN_COST double, SHIP_COST double, SELL_COST double, ' \
-                                      'DIFF_ACT_PRE_MAN_COST double, IDLE_EQUIP_COST double, RD_COST double, ' \
-                                      'ADMIN_COST_BU double, ADMIN_COST_OD double, OTHER_OP_COST_BU double, ' \
-                                      'OTHER_OP_COST_OD double, SPEC_ITEMS double, PROVISIONS double, CURRENCY_GAINS double, ' \
-                                      'VAL_ADJUST_INVENTORIES double, OTHER_FIX_COST double, TOPDOWN_ADJUST_FIX_COSTS double, ' \
-                                      'DEPRECIATION double, CAP_COST double, SBU varchar, REGION varchar, SUBREGION varchar, ' \
-                                      'ENTRY_TYPE varchar, PERIOD int, PERIOD_YEAR int, PERIOD_MONTH int, PLAN_PERIOD int, ' \
-                                      'PLAN_YEAR int, PLAN_HALF_YEAR int, PLAN_QUARTER int, PLAN_MONTH int, CURRENCY varchar, ' \
-                                      'STATUS varchar, USERCOMMENT text, USERID varchar, ENTRY_TS varchar, ' \
-                                      'ADMIN_COST_COMPANY double, OTHER_OP_COST_COMPANY double, EQUITY_INCOME double, ' \
-                                      'PRIMARY KEY((SBU, SUBREGION), PERIOD, ENTRY_TYPE, PLAN_PERIOD));'
-
 CREATE_KEY_SPACE = "CREATE KEYSPACE IF NOT EXISTS"
-CREATE_KEY_SPACE_SUB = "WITH REPLICATION = { 'class' : 'SimpleStrategy', 'replication_factor' : 3 };"
+CREATE_KEY_SPACE_SUB = "WITH REPLICATION = { 'class' : 'SimpleStrategy', 'replication_factor' : 3 }"
 CREATE_TABLE = "CREATE TABLE IF NOT EXISTS"
 IMPORT_FILE = 'COPY'
 IMPORT_FILE_SUB = 'FROM'
@@ -96,25 +37,8 @@ DELETE_KEYSPACE = 'DROP KEYSPACE IF EXISTS'
 DELETE_TABLE = 'DROP TABLE IF EXISTS'
 
 # Allgemeine Parameter
-table_params = {
-    'ORG_STRUCTURE': ORG_STRUCTURE_PARAMS,
-    'REGIONS': REGIONS_PARAMS,
-    'EXCHANGE_RATE': EXCHANGE_RATE_PARAMS,
-    'ACTUAL_SALES': ACTUAL_SALES_PARAMS,
-    'FORECAST_SALES': FORECAST_SALES_PARAMS,
-    'FORECAST_FIXED_COSTS': FORECAST_FIXED_COSTS_PARAMS,
-    'ACTUAL_FIXED_COSTS': ACTUAL_FIXED_COSTS_PARAMS,
-}
 
-table_creation_params = {
-    'ORG_STRUCTURE': ORG_STRUCTURE_TABLE_CREATION,
-    'REGIONS': REGIONS_TABLE_CREATION,
-    'EXCHANGE_RATE': EXCHANGE_RATE_TABLE_CREATION,
-    'ACTUAL_SALES': ACTUAL_SALES_TABLE_CREATION,
-    'FORECAST_SALES': FORECAST_SALES_TABLE_CREATION,
-    'FORECAST_FIXED_COSTS': FORECAST_FIXED_COSTS_TABLE_CREATION,
-    'ACTUAL_FIXED_COSTS': ACTUAL_FIXED_COSTS_TABLE_CREATION,
-}
+PATH_CSV_OUTPUT = os.path.dirname(__file__) + '/csv_output/'
 
 
 # Funktion für Parser
@@ -137,7 +61,7 @@ parser.add_argument('--dtable', '-t', action='append', help='table witch should 
 parser.add_argument('--dkeyspace', '-d', action="store_true", help='Keyspace should be deleted')
 parser.add_argument('--inputfile', '-i', action='append', type=inputpath_and_tablename, nargs='*',
                     help='Input files for tables. Format inputpath,tablename. Following Tablenames are valid: %s'
-                         % table_params.keys())
+                    % config['available_params'])
 
 args = parser.parse_args()
 
@@ -154,9 +78,9 @@ def check_verbose():
 
 def run_query(query):
     if args.user is None and args.password is None:
-        return subprocess.call([CQLSH_BINARY, args.ip, query])
+        return subprocess.call([CQLSH, args.ip, query])
     else:
-        return subprocess.call(CQLSH_BINARY + ' ' + args.ip + ' ' + query, shell=True)
+        return subprocess.call(CQLSH + ' ' + args.ip + ' ' + query, shell=True)
 
 
 def validate_path(file_path):
@@ -176,16 +100,16 @@ def validate_path(file_path):
 
 def validate_tablenames(tablename):
     try:
-        table_params[tablename]
+        config['table_params'][tablename]
     except KeyError:
         print('The tablename %s is not valid.' % tablename)
-        exit('Canceled: please use the following tablenames: %s' % table_params.keys())
+        exit('Canceled: please use the following tablenames: %s' % config['available_params'])
 
 
 def validate_csv(name, reference_file):
     reader = csv.reader(open(PATH_CSV_OUTPUT + name, 'r'), delimiter=',')
     header_from_csv = next(reader)
-    if header_from_csv == table_params[reference_file].upper().split(', '):
+    if header_from_csv == config['table_params'][reference_file].upper().split(', '):
         if check_verbose() == 1:
             print ('The header from %s.csv is valid.' % name)
         return True
@@ -234,7 +158,7 @@ def test_databaseconnection():
         else:
             exit('Database connection to %s is not possible!' % args.ip)
     else:
-        connection_test_param = USER + '"' + args.user + '"' + ' ' + PASSWORD + '"' + args.password + '"' + ' ' + INTERACTIVE_MODE + 'EXIT'
+        connection_test_param = USER + args.user + ' ' + PASSWORD + args.password + ' ' + INTERACTIVE_MODE + 'EXIT'
         databaseconnection_exit_code = run_query(connection_test_param)
         if databaseconnection_exit_code == 0:
             if check_verbose() >= 1:
@@ -251,7 +175,8 @@ def create_keyspace():
             print('Datatbase %s was created or already exists.' % args.keyspace)
     else:
         temp_parameters = USER + '"' + args.user + '"' + ' ' + PASSWORD + '"' + args.password + '"' + ' ' + \
-                          INTERACTIVE_MODE + ' ' + CREATE_KEY_SPACE + ' ' + args.keyspace + ' ' + CREATE_KEY_SPACE_SUB
+                          INTERACTIVE_MODE + ' ' + '"' + CREATE_KEY_SPACE + ' ' + args.keyspace + ' ' + \
+                          CREATE_KEY_SPACE_SUB + '"'
         run_query(temp_parameters)
         if check_verbose() >= 1:
             print('Datatbase %s was created or already exists.' % args.keyspace)
@@ -265,7 +190,7 @@ def delete_keyspace():
             print('Keyspace %s was deleted.' % args.keyspace)
     else:
         temp_parameters = USER + '"' + args.user + '"' + ' ' + PASSWORD + '"' + args.password + '"' + ' ' + \
-                          INTERACTIVE_MODE + ' ' + DELETE_KEYSPACE + ' ' + args.keyspace
+                          INTERACTIVE_MODE + ' ' + '"' + DELETE_KEYSPACE + ' ' + args.keyspace + '"'
         run_query(temp_parameters)
         if check_verbose() >= 1:
             print('Keyspace %s was deleted.' % args.keyspace)
@@ -279,12 +204,12 @@ def create_table(tablename, table_parameter):
         if check_verbose() >= 1:
             print('Table %s was created or already exists.' % tablename)
     else:
-        temp_parameters = USER + '"' + args.user + '"' + ' ' + PASSWORD + '"' + args.password + '"' + ' ' + \
-                          INTERACTIVE_MODE + ' ' + CREATE_TABLE + ' ' + args.keyspace + '.' + \
-                          tablename + table_parameter
+        temp_parameters = USER + '"' + args.user + '"' + ' ' + PASSWORD + '"' + args.password + '"' + ' ' + '"' \
+                          + INTERACTIVE_MODE + ' ' + CREATE_TABLE + ' ' + args.keyspace + '.' + \
+                          tablename + table_parameter + '"'
         run_query(temp_parameters)
         if check_verbose() >= 1:
-            print('Datatbase %s was created or already exists.' % args.keyspace)
+            print('Table %s was created or already exists.' % tablename)
 
 
 def delete_table(tablename):
@@ -294,7 +219,7 @@ def delete_table(tablename):
         if check_verbose() >= 1:
             print('Table %s was deleted.' % tablename)
     else:
-        temp_parameters = USER + '"' + args.user + '"' + ' ' + PASSWORD + '"' + args.password + '"' + ' ' + \
+        temp_parameters = USER + '"' + args.user + '"' + ' ' + PASSWORD + '"' + args.password + '"' + ' ' + '"' +\
                           INTERACTIVE_MODE + ' ' + DELETE_TABLE + ' ' + args.keyspace + '.' + tablename
         run_query(temp_parameters)
         if check_verbose() >= 1:
@@ -308,21 +233,21 @@ def import_file(tablename, import_params, import_path):
                           "'" + import_path + "'" + ' ' + IMPORT_FILE_SUB_SUB
         if check_verbose() == 1:
             with open(os.devnull, "w") as f:
-                subprocess.call([CQLSH_BINARY, args.ip, temp_parameters], stdout=f)
+                subprocess.call([CQLSH, args.ip, temp_parameters], stdout=f)
                 print('File %s was successfully imported' % tablename)
         if check_verbose() == 2:
-            subprocess.call([CQLSH_BINARY, args.ip, temp_parameters])
+            subprocess.call([CQLSH, args.ip, temp_parameters])
     else:
-        temp_parameters = USER + '"' + args.user + '"' + ' ' + PASSWORD + '"' + args.password + '"' + ' ' + \
+        temp_parameters = USER + '"' + args.user + '"' + ' ' + PASSWORD + '"' + args.password + '"' + ' ' + '"' +\
                           INTERACTIVE_MODE + ' ' + IMPORT_FILE + ' ' + args.keyspace + '.' + tablename + \
                           '(' + import_params + ') ' + IMPORT_FILE_SUB + \
-                          "'" + import_path + "'" + ' ' + IMPORT_FILE_SUB_SUB
+                          "'" + import_path + "'" + ' ' + IMPORT_FILE_SUB_SUB + '"'
         if check_verbose() == 1:
             with open(os.devnull, "w") as f:
-                subprocess.call(CQLSH_BINARY + ' ' + args.ip + ' ' + temp_parameters, shell=True, stdout=f)
+                subprocess.call(CQLSH + ' ' + args.ip + ' ' + temp_parameters, shell=True, stdout=f)
                 print('File %s was successfully imported' % tablename)
         if check_verbose() == 2:
-            subprocess.call(CQLSH_BINARY + ' ' + args.ip + ' ' + temp_parameters, shell=True)
+            subprocess.call(CQLSH + ' ' + args.ip + ' ' + temp_parameters, shell=True)
 
 
 def transformation_and_validation():
@@ -371,14 +296,14 @@ def fileimport():
     # Legt die Tabellen an
     for val in firstlistelement_of_inputfiles:
         second_value_from_tuple = val[1]
-        create_table(second_value_from_tuple, table_creation_params[second_value_from_tuple])
+        create_table(second_value_from_tuple, config['table_creation_params'][second_value_from_tuple])
 
     # Importiert die Files
     for val in firstlistelement_of_inputfiles:
         csv_output_list = os.listdir(PATH_CSV_OUTPUT)
         second_value_from_tuple = val[1]
         csv_path = PATH_CSV_OUTPUT + csv_output_list[csv_output_list.index(second_value_from_tuple + '.csv')]
-        import_file(second_value_from_tuple, table_params[second_value_from_tuple], csv_path)
+        import_file(second_value_from_tuple, config['table_params'][second_value_from_tuple], csv_path)
 
 
 def create_csv_folder():
