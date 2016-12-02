c0dfb3b1315d4d657aa8c96c057a5fa9d3ef06fd
wagnst <mail@steffenwagner.com>
2016-12-02 13:01:25 +0100

    Removed unused method

--- src/main/java/fourschlag/jsoncompare/JsonDiff.java
+++ src/main/java/fourschlag/jsoncompare/JsonDiff.java
@@ -1,12 +1,7 @@
 package fourschlag.jsoncompare;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import fourschlag.entities.types.Currency;
 import fourschlag.entities.types.OutputDataType;
-import fourschlag.entities.types.comparators.OutputDataTypeComparator;
-import fourschlag.services.data.services.FixedCostsService;
-import fourschlag.services.data.services.SalesService;
-import fourschlag.services.db.CassandraConnection;
 
 import java.io.File;
 import java.io.IOException;
@@ -15,13 +10,8 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class JsonDiff {
-    private static CassandraConnection connection = CassandraConnection.getInstance();
-    private static SalesService salesService = new SalesService(connection);
-    private static FixedCostsService fixedCostsService = new FixedCostsService(connection);
-
     public static void main(String[] args) {
         Set<CompareObject> ourSet = getJsonFromFile(new File("resultGorilla.json"));
         Set<CompareObject> theirSet = getJsonFromFile(new File("spJson.json"));
@@ -68,20 +58,18 @@ public class JsonDiff {
 
     }
 
-    static List<CompareObject> getSortedList(Set<CompareObject> set) {
+    private static List<CompareObject> getSortedList(Set<CompareObject> set) {
         return set.stream()
                 .sorted(new DiffComparator())
                 .collect(Collectors.toList());
     }
 
-    static Set<CompareObject> getJsonFromFile(File json) {
+    private static Set<CompareObject> getJsonFromFile(File json) {
         ObjectMapper om = new ObjectMapper();
 
-        File spJsonFile = json;
-
-        OutputDataType[] spEntries = null;
+        OutputDataType[] spEntries = new OutputDataType[]{};
         try {
-            spEntries = om.readValue(spJsonFile, OutputDataType[].class);
+            spEntries = om.readValue(json, OutputDataType[].class);
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -90,32 +78,5 @@ public class JsonDiff {
                 .map(entry -> new CompareObject(entry.getSbu(), entry.getProductMainGroup(), entry.getRegion(),entry.getSubregion(),
                         entry.getSalesType(), entry.getEntryType()))
                 .collect(Collectors.toSet());
-    }
-
-    /* Only for Team FourSchlag. If you are not Team FourSchlag, then use getJsonFromFile two times */
-    static Set<CompareObject> getOurJson() {
-        int planYear = 2016;
-        int period = 201609;
-        Currency curr = Currency.getCurrencyByAbbreviation("usd");
-
-        /* Get all Sales KPIs and save them to a stream */
-        Stream<OutputDataType> salesKpis = salesService.getSalesKPIs(planYear, period, curr);
-        /* Also save the Fixed Costs KPIs to a stream */
-        Stream<OutputDataType> fixedCostsKpis = fixedCostsService.getFixedCostsKpis(planYear, period, curr);
-
-        /* Combine both streams to one */
-        Set<CompareObject> resultSet = Stream.concat(salesKpis, fixedCostsKpis)
-                /* Sort the whole stream */
-                .sorted(new OutputDataTypeComparator())
-                /* Convert the stream to a List */
-                .map(entry -> new CompareObject(entry.getSbu(), entry.getProductMainGroup(), entry.getRegion(),entry.getSubregion(),
-                entry.getSalesType(), entry.getEntryType()))
-                .collect(Collectors.toSet());
-
-        /* Close both streams */
-        salesKpis.close();
-        fixedCostsKpis.close();
-
-        return resultSet;
     }
 }
