dbfbd281b1d928377b08500ccd2a7cd764cf896c
wagnst <mail@steffenwagner.com>
2016-12-05 14:48:32 +0100

    Improved program

--- src/main/java/fourschlag/jsoncompare/JsonDiff.java
+++ src/main/java/fourschlag/jsoncompare/JsonDiff.java
@@ -10,54 +10,58 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class JsonDiff {
     public static void main(String[] args) {
         /* Get both JSON Files and put all entries into a Set to remove all duplicates */
-        Set<CompareObject> ourSet = getJsonFromFile(new File("resultJson.json"));
-        Set<CompareObject> theirSet = getJsonFromFile(new File("spJson.json"));
+
+        Stream<OutputDataType> ourJson = getJsonFromFile(new File("resultJson.json"));
+        Stream<OutputDataType> spJson = getJsonFromFile(new File("spJson.json"));
+
+        Set<CompareObject> ourSet;
+        Set<CompareObject> spSet;
+        boolean withKpi = false;
+        if (args.length == 1) {
+            if (args[0].equals("true")) {
+                withKpi = true;
+            }
+        }
+
+        if (withKpi) {
+            ourSet = asSetWithKpi(ourJson);
+            spSet = asSetWithKpi(spJson);
+        } else {
+            ourSet = asSetWithoutKpi(ourJson);
+            spSet = asSetWithoutKpi(spJson);
+        }
 
         /* Sort the collection to make it easier to read later */
         List<CompareObject> ourList = getSortedList(ourSet);
-        List<CompareObject> theirList = getSortedList(theirSet);
+        List<CompareObject> spList = getSortedList(spSet);
 
         List<CompareObject> noDuplicates = new ArrayList<>();
-        System.out.println("Checking if our list contains every object from their list");
+        System.out.println("The following entries are part of the SP Json, but are missing in your Json:\n");
         /* For each object in their List check
          * IF our List does not contain it
          * THEN add it to our result list
          */
-        for (CompareObject param : theirList) {
+        for (CompareObject param : spList) {
             if (!ourList.contains(param)) {
                 noDuplicates.add(param);
             }
         }
-
-        /*
-        System.out.println("Checking if their list contains every object from our list");
-        for (CompareObject param : ourList) {
-            if (!theirList.contains(param)) {
-                noDuplicates.add(param);
-            }
-        } */
-
 
         /* Print out the result list */
         for (CompareObject object : noDuplicates) {
             System.out.println(object);
         }
 
-        System.out.println("Finished");
-
-        /*
-        for (CompareObject param : ourList) {
-            System.out.println(param.toString());
-        }
-         */
+        System.out.println("\n\nFinished\n");
 
 
-        System.out.println("Size of our list: " + ourList.size());
-        System.out.println("Size of their list: " + theirList.size());
+        System.out.println("Number of entries/combinations in your Json (without duplicates): " + ourList.size());
+        System.out.println("Number of entries/combinations in SP Json (without duplicates): " + spList.size());
 
     }
 
@@ -67,7 +71,7 @@ public class JsonDiff {
                 .collect(Collectors.toList());
     }
 
-    private static Set<CompareObject> getJsonFromFile(File json) {
+    private static Stream<OutputDataType> getJsonFromFile(File json) {
         ObjectMapper om = new ObjectMapper();
 
         OutputDataType[] spEntries = new OutputDataType[]{};
@@ -77,9 +81,21 @@ public class JsonDiff {
             e.printStackTrace();
         }
 
-        return Arrays.stream(spEntries)
+        return Arrays.stream(spEntries);
+    }
+
+    private static Set<CompareObject> asSetWithoutKpi(Stream<OutputDataType> stream) {
+        return stream
                 .map(entry -> new CompareObject(entry.getSbu(), entry.getProductMainGroup(), entry.getRegion(), entry.getSubregion(),
                         entry.getSalesType(), entry.getEntryType()))
                 .collect(Collectors.toSet());
     }
+
+    private static Set<CompareObject> asSetWithKpi(Stream<OutputDataType> stream) {
+        return stream
+                .map(entry -> new CompareObjectWithKpi(entry.getSbu(), entry.getProductMainGroup(), entry.getRegion(), entry.getSubregion(),
+                        entry.getSalesType(), entry.getEntryType(), entry.getKpi()))
+                .collect(Collectors.toSet());
+    }
+
 }
