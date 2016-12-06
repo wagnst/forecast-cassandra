ada6e1a65a61e808e269472a2d6e3d7f0c03b6f6
wagnst <mail@steffenwagner.com>
2016-12-06 11:45:48 +0100

    Removed unused method

--- src/main/java/fourschlag/entities/accessors/OrgStructureAccessor.java
+++ src/main/java/fourschlag/entities/accessors/OrgStructureAccessor.java
@@ -2,7 +2,6 @@ package fourschlag.entities.accessors;
 
 import com.datastax.driver.mapping.Result;
 import com.datastax.driver.mapping.annotations.Accessor;
-import com.datastax.driver.mapping.annotations.Param;
 import com.datastax.driver.mapping.annotations.Query;
 import fourschlag.entities.tables.OrgStructureEntity;
 
@@ -15,8 +14,4 @@ public interface OrgStructureAccessor {
     /*CQL-Query to get the OrgStructure data */
     @Query("SELECT product_main_group, sbu FROM org_structure;")
     Result<OrgStructureEntity> getProductsAndSbus();
-
-    /*CQL-Query to get the sbu for a specific Product Main Group */
-    @Query("SELECT sbu FROM org_structure WHERE product_main_group = :product_main_group;")
-    OrgStructureEntity getSbuForProductMainGroup(@Param("product_main_group") String productMainGroup);
 }
