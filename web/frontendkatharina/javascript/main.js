ecef5fb5f8d402c97746676852addfe92267e3f5
Rahtainka <rahtainka@t-online.de>
2016-12-06 11:57:28 +0100

    added contend

--- web/frontendkatharina/javascript/main.js
+++ web/frontendkatharina/javascript/main.js
@@ -3,5 +3,35 @@
  */
 
 $(document).ready(function() {
-    $('#example').DataTable();
+    $('#example').DataTable( {
+        "ajax": {
+            "url": "fourschlag/api/forecast/"
+        },
+        "columns": [
+            { "data": "PERIOD" },
+            { "data": "REGION" },
+            { "data": "PERIOD_YEAR" },
+            { "data": "PERIOD_MONTH" },
+            { "data": "CURRENCY" },
+            { "data": "USERID" }
+        ]
+    } );
 } );
+/*
+$(document).ready(function() {
+    $('#example').dataTable({
+        "ajax": function (data, callback, settings) {
+            callback(
+                JSON.parse(localStorage.getItem('test.json'))
+            );
+        },
+        "columns": [
+            {"data": "PERIOD"},
+            {"data": "REGION"},
+            {"data": "PERIOD_YEAR"},
+            {"data": "PERIOD_MONTH"},
+            {"data": "CURRENCY"},
+            {"data": "USERID"}
+        ]
+    });
+});*/
