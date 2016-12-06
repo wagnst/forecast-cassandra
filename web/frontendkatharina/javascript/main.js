09853e6c2c6c9ca071100403ed1c4e6e5ca4149b
Rahtainka <rahtainka@t-online.de>
2016-12-06 18:50:29 +0100

    added basic funktions for KPI Request

--- web/frontendkatharina/javascript/main.js
+++ web/frontendkatharina/javascript/main.js
@@ -2,36 +2,98 @@
  * Created by katharinaspinner on 05.12.16.
  */
 
-$(document).ready(function() {
-    $('#example').DataTable( {
+/*$(document).ready(function () {
+     $('#example').DataTable({
         "ajax": {
-            "url": "fourschlag/api/forecast/"
+            "url":"http://localhost:8080/fourschlag/api/forecast/usd/2013/201611",
+            "dataSrc": ""
         },
         "columns": [
-            { "data": "PERIOD" },
-            { "data": "REGION" },
-            { "data": "PERIOD_YEAR" },
-            { "data": "PERIOD_MONTH" },
-            { "data": "CURRENCY" },
-            { "data": "USERID" }
-        ]
-    } );
-} );
-/*
-$(document).ready(function() {
-    $('#example').dataTable({
-        "ajax": function (data, callback, settings) {
-            callback(
-                JSON.parse(localStorage.getItem('test.json'))
-            );
-        },
-        "columns": [
-            {"data": "PERIOD"},
+            {"data": "ENTRY_TYPE"},
             {"data": "REGION"},
-            {"data": "PERIOD_YEAR"},
-            {"data": "PERIOD_MONTH"},
-            {"data": "CURRENCY"},
-            {"data": "USERID"}
+            {"data": "SUBREGION"},
+            {"data": "SBU"},
+            {"data": "PRODUCT_MAIN_GROUP"},
+            {"data": "SALES_TYPE"},
+            {"data": "FC_TYPE"},
+            {"data": "ORDER_NUMBER"},
+            {"data": "KPI"},
+            {"data": "UNIT"},
+            {"data": "M01"},
+            {"data": "M02"},
+            {"data": "M03"},
+            {"data": "M04"},
+            {"data": "M05"},
+            {"data": "M06"},
+            {"data": "M07"},
+            {"data": "M08"},
+            {"data": "M09"},
+            {"data": "M10"},
+            {"data": "M11"},
+            {"data": "M12"},
+            {"data": "M13"},
+            {"data": "M14"},
+            {"data": "M15"},
+            {"data": "M16"},
+            {"data": "M17"},
+            {"data": "M18"},
+            {"data": "BJ2"},
+            {"data": "BJ3"},
+            {"data": "BJ4"}
         ]
     });
 });*/
+
+var table = $('#example').DataTable({
+        "ajax": {
+            "url":"test.json",
+            "dataSrc": ""
+        },
+        "info": false,
+        aaSorting: [],
+        "columns": [
+            {"data": "ENTRY_TYPE"},
+            {"data": "REGION"},
+            {"data": "SUBREGION"},
+            {"data": "SBU"},
+            {"data": "PRODUCT_MAIN_GROUP"},
+            {"data": "SALES_TYPE"},
+            {"data": "FC_TYPE"},
+            {"data": "ORDER_NUMBER"},
+            {"data": "KPI"},
+            {"data": "UNIT"},
+            {"data": "M01"},
+            {"data": "M02"},
+            {"data": "M03"},
+            {"data": "M04"},
+            {"data": "M05"},
+            {"data": "M06"},
+            {"data": "M07"},
+            {"data": "M08"},
+            {"data": "M09"},
+            {"data": "M10"},
+            {"data": "M11"},
+            {"data": "M12"},
+            {"data": "M13"},
+            {"data": "M14"},
+            {"data": "M15"},
+            {"data": "M16"},
+            {"data": "M17"},
+            {"data": "M18"},
+            {"data": "BJ2"},
+            {"data": "BJ3"},
+            {"data": "BJ4"}
+        ]
+    });
+
+
+
+window.addEventListener('load', function(){
+    var startbutton = document.getElementById('startbutton')
+    startbutton.addEventListener('click', function(){
+        table.ajax.url( 'http://localhost:8080/fourschlag/api/forecast/usd/2013/201611' ).load();
+    }, false);
+}, false);
+
+
+
