c65fbe487e10060602f31351e4f03ea84cd07b0c
wagnst <mail@steffenwagner.com>
2016-12-07 17:01:05 +0100

    implemented parameter validator for http requests

---
+++ src/main/java/fourschlag/services/web/ws/ParameterValidator.java
@@ -0,0 +1,11 @@
+package fourschlag.services.web.ws;
+
+/**
+ * Copyright by Steffen Wagner (steffen)
+ * Created on 07.12.16, 15:11
+ * Project: fourschlag
+ * Package: fourschlag.services.web.ws
+ */
+
+public class ParameterValidator {
+}
